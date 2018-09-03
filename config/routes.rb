86b098073820386b42a80a932506a963faf504d8
github username <newmanbradm@gmail.com>
2018-09-03 14:29:42 -0400

    update user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, only: [:index, :create, :update]
       resources :events, only: [:index, :create, :update, :destroy] do
         resources :supplies, only: [:index, :create, :update, :destroy]
         resources :posts, only: [:index, :create, :update, :destroy]
