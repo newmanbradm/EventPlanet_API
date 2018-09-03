80fd78276b6d09eb0bb7f89a46c603459114decc
github username <newmanbradm@gmail.com>
2018-09-03 14:28:59 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :events do
-        resources :supplies
-        resources :posts
-        resources :comments
+      resources :events, only: [:index, :create, :update, :destroy] do
+        resources :supplies, only: [:index, :create, :update, :destroy]
+        resources :posts, only: [:index, :create, :update, :destroy]
+        resources :comments, only: [:index, :create, :update, :destroy]
       end
-      resources :venues
-      resources :event_venues
-      resources :guests
-      resources :event_guests
+      resources :venues, only: [:index, :create, :update]
+      resources :event_venues, only: [:index, :create, :update, :destroy]
+      resources :guests, only: [:index, :create, :update]
+      resources :event_guests, only: [:index, :create, :update, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
